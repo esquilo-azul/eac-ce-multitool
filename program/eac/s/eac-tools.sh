2408022087869bed345fd51693d4b9e4a04c0dbc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-05-01 18:50:54 -0300

    Program[m/*]: move de "eac/m/*".

--- program/eac/s/eac-tools.sh
+++ program/eac/s/eac-tools.sh
@@ -2,4 +2,4 @@
 
 source "$MYSELF_LIB/bash/init.sh"
 
-"$PROGRAMEIRO_RUNNER" /eac/m/sources/ruby/gems/exe_wrapper eac_tools "$EAC_TOOLS_DEV_INSTALL_PATH" eac "$@"
+"$PROGRAMEIRO_RUNNER" /m/sources/ruby/gems/exe_wrapper eac_tools "$EAC_TOOLS_DEV_INSTALL_PATH" eac "$@"
